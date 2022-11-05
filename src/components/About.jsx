9db39ac3e7d17b3ae6e061e74ef859d76af127be
syed-mesam-ali <syedmesamali@gmail.com>
2022-11-05 23:20:41 +0530

    mobile fixed home and about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,13 +6,13 @@ const About = () => {
       name="about"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full sm:h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
-        <p className="text-xl mt-20 ">
+        <p className="text-xl md:mt-20 ">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor,
           dolores beatae, illum eum minima similique enim magnam alias, culpa
           rem aliquam eaque unde exercitationem. Quisquam tempora laudantium
