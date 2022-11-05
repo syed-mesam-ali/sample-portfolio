3b2f6d433501eec0088f0091bbcfe8b7075758ff
syed-mesam-ali <syedmesamali@gmail.com>
2022-11-05 22:36:15 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20 ">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor,
+          dolores beatae, illum eum minima similique enim magnam alias, culpa
+          rem aliquam eaque unde exercitationem. Quisquam tempora laudantium
+          expedita eius ad fugit distinctio modi repellendus voluptas quae iure,
+          voluptatibus, officia a exercitationem omnis. Laborum in amet cum
+          itaque ut doloribus ipsum.
+        </p>
+        <br />
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          error beatae at dolore molestiae facilis exercitationem doloremque.
+          Atque, vero asperiores rerum nostrum aperiam dolore eaque sequi aut
+          reiciendis harum itaque ut eligendi pariatur ducimus! Vel rem
+          cupiditate optio? Iusto quibusdam alias earum veniam consequatur
+          aliquid asperiores sunt rem facilis modi?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import css from "../assets/css.png";
+import javascript from "../assets/javascript.png";
+import graphql from "../assets/graphql.png";
+import tailwind from "../assets/tailwind.png";
+import nextjs from "../assets/nextjs.png";
+import reactImage from "../assets/react.png";
+import node from "../assets/node.png";
+import html from "../assets/html.png";
+
+const Experience = () => {
+  const skills = [
+    {
+      id: 1,
+      source: html,
+      title: "HTML",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 2,
+      source: css,
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 3,
+      source: javascript,
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      source: reactImage,
+      title: "React",
+      style: "shadow-blue-600",
+    },
+    {
+      id: 5,
+      source: tailwind,
+      title: "Tailwind",
+      style: "shadow-sky-400",
+    },
+    {
+      id: 6,
+      source: nextjs,
+      title: "Nextjs",
+      style: "shadow-white",
+    },
+    {
+      id: 7,
+      source: graphql,
+      title: "GraphQL",
+      style: "shadow-pink-400",
+    },
+    {
+      id: 8,
+      source: node,
+      title: "Node",
+      style: "shadow-green-500",
+    },
+  ];
+
+  return (
+    <div
+      name="experience"
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+    >
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+        <div>
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline ">
+            Experience
+          </p>
+          <p className="py-6 ">These are the technologies I've worked with</p>
+        </div>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0 ">
+          {skills.map(({ id, source, title, style }) => (
+            <div
+              key={id}
+              className={
+                "shadow-md hover:scale-105 duration-500 py-2 rounded-lg" +
+                " " +
+                style
+              }
+            >
+              <img src={source} alt="" className="w-20 mx-auto" />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
