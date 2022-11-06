b04994b37feedf63796f04b734215a91bb4af24b
syed-mesam-ali <syedmesamali@gmail.com>
2022-11-06 13:07:17 +0530

    offset set in portfolio button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,6 +23,7 @@ const Home = () => {
               to={"portfolio"}
               smooth
               duration={700}
+              offset={window.screen.width > 768 ? -30 : -70}
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500  to-blue-500 cursor-pointer"
             >
               Portfolio
